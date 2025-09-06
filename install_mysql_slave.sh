87b49ebd24eb473f0bf4a74dde77a7b47b440210
Zlodim89 <belousov.d.v.dom@gmail.com>
2025-09-06 23:59:25 +0300

    Update install_mysql_slave.sh

--- install_mysql_slave.sh
+++ install_mysql_slave.sh
@@ -14,44 +14,9 @@ echo "Установка MySQL"
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y mysql-server
 
-# === Настройка my.cnf для SLAVE ===
-echo "Настройка MySQL как SLAVE"
-sudo tee /etc/mysql/mysql.conf.d/99-replication.cnf > /dev/null <<EOF
-[mysqld]
-server-id=${SERVER_ID}
-bind-address=${BIND_ADDRESS}
-
-# Включаем GTID
-gtid_mode=ON
-enforce-gtid-consistency=ON
-EOF
-
-# === Перезапуск MySQL ===
-echo "[Перезапуск MySQL"
-sudo systemctl restart mysql
-
 # === Настройка root-пароля ===
 echo "Настройка root пользователя"
 mysql --user=root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-
-# === Подключение к мастеру ===
-echo "Подключение к мастеру (${MASTER_HOST})"
-mysql --user=root -p${MYSQL_ROOT_PASSWORD} <<EOF
-STOP REPLICA;
-RESET REPLICA ALL;
-
-CHANGE REPLICATION SOURCE TO
-  SOURCE_HOST='${MASTER_HOST}',
-  SOURCE_USER='${REPL_USER}',
-  SOURCE_PASSWORD='${REPL_PASSWORD}',
-  SOURCE_AUTO_POSITION=1;
-
-START REPLICA;
-EOF
-
-# === Проверка статуса реплики ===
-echo "Статус репликации"
-mysql --user=root -p${MYSQL_ROOT_PASSWORD} -e "SHOW REPLICA STATUS\G" | egrep 'Running|Auto_Position'
